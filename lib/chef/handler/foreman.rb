a13355432224409a58b39472ad74e0a0cc1968ad
Romain Vrignaud <rvrignaud@gmail.com>
2013-09-30 14:58:26 +0000

    Use a private function

--- lib/chef/handler/foreman.rb
+++ lib/chef/handler/foreman.rb
@@ -1,3 +1,16 @@
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 require 'chef'
 require 'chef/handler'
 require 'net/http'
@@ -7,24 +20,13 @@ require 'uri'
 class ForemanReporting < Chef::Handler
         attr_reader :options 
 	def initialize ( opts = {})
-		@options = {
-			:foreman_url => 'http://foreman.fitzdsl.net:3000'
-		}
-
+		#Default report values
+		@options = {}
 		@options.merge! opts
 	end
 	
 	METRIC = %w[applied restarted failed failed_restarts skipped pending]
 	def report
-		Chef::Log.debug("Run status is #{run_status.inspect}")
-
-		uri = URI.parse(options[:foreman_url])
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl     = uri.scheme == 'https'
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-		req = Net::HTTP::Post.new("#{uri.path}/api/reports")
-		req.add_field('Accept', 'application/json,version=2' )
-		req.content_type = 'application/json'
 
 		report = {}
 		report['host'] = node.fqdn
@@ -33,32 +35,66 @@ class ForemanReporting < Chef::Handler
 		METRIC.each do |m|
 		        report_status[m] = 0
 		end
+		if failed?
+			report_status['failed'] = 1
+		end
 		report['status'] = report_status
+
+		# I don't know what metrics is used for
 		metrics = {}
-		metrics['applied'] = 1
-		metrics['restarted'] = 0
-		metrics['failed'] = 0
-		metrics['failed_restarts'] = 0
-		metrics['skipped'] = 0
-		metrics['pending'] = 0
 		report['metrics'] =  metrics
 
 		logs = []
-		l = { 'log' => { 'sources' => {}, 'messages' => {} } }
-		l['log']['level'] = 'notice'
-		l['log']['messages']['message'] = "Chef has done this"
-		l['log']['sources']['source'] = 'Chef'
-		logs << l
+		run_status.updated_resources.each  do |resource|
+			l = { 'log' => { 'sources' => {}, 'messages' => {} } }
+			l['log']['level'] = 'notice'
+			l['log']['messages']['message'] = resource.action.to_s
+			l['log']['sources']['source'] = [resource.class.to_s,resource.name].join(' ')
+			logs << l
+		end
+
+		# I only set failed to 1 if chef run failed
+		if failed?
+			l = { 'log' => { 'sources' => {}, 'messages' => {} } }
+			l['log']['level'] = 'err'
+			l['log']['sources']['source'] = 'chef'
+			l['log']['messages']['message'] = run_status.exception
+			logs << l
+		end
+
 		report['logs'] = logs
 		full_report =  { 'report' => report}
 
-
-		Chef::Log.info("Report is #{full_report.inspect}")
-
-		req.body = full_report.to_json
-		response = http.request(req)
+		send_report(full_report)
 	end
 
+	private
+
+	def send_report (report)
+		uri = URI.parse(options[:foreman_url])
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl     = uri.scheme == 'https'
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+
+		if http.use_ssl?
+			if options[:foreman_ssl_ca] && !options[:foreman_ssl_ca].empty?
+			  http.ca_file = options[:foreman_ssl_ca]
+			  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+			else
+			  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+			end
+			if options[:foreman_ssl_cert] && !options[:foreman_ssl_cert].empty? && options[:foreman_ssl_key] && !options[:foreman_ssl_key].empty?
+			  http.cert = OpenSSL::X509::Certificate.new(File.read(options[:foreman_ssl_cert]))
+			  http.key  = OpenSSL::PKey::RSA.new(File.read(options[:foreman_ssl_key]), nil)
+			end
+		end
+		req = Net::HTTP::Post.new("#{uri.path}/api/reports")
+		req.add_field('Accept', 'application/json,version=2' )
+		req.content_type = 'application/json'
+		req.body = report.to_json
+		response = http.request(req)
+	end
 end
 
 
